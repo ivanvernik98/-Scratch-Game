bd762886396871c85b8ad05491a8bd1a7fff807d
Bimbola Faminu <150906108+ivanvernik98@users.noreply.github.com>
2024-02-04 19:20:04 +0530

    Create  Scratch Game.swift

---
+++ Scratch Game.swift
@@ -0,0 +1,7 @@
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
+    <title>JavaScript Scratch Game</title>
+</head>
+<body>
+    <script src="game.js"></script>
+</body>
+</html>
